03f1f5845fe8134a42741f814010e0dc23e06246
Glenn Ricaud <gri@enonic.com>
2018-04-01 18:25:17 +0200

    League data cache (#8)

--- src/main/resources/services/league-data/league-data.js
+++ src/main/resources/services/league-data/league-data.js
@@ -26,72 +26,89 @@ exports.post = function (req) {
 
 function getData(leagueId) {
     var playersData = {};
+    var teamsData = {};
     var gameDates = [];
-    getPlayerRankingData(leagueId, playersData);
-    getPlayersData(playersData);
-    getPlayerRatingData(leagueId, playersData, gameDates);
+    getEntityRankingData(leagueId, playersData, 'Player');
+    getEntityRankingData(leagueId, teamsData, 'Team');
+    getEntitiesData(playersData, 'Player');
+    getEntitiesData(teamsData, 'Team');
+    getEntityRatingData(leagueId, playersData, teamsData, gameDates);
     
     return {
         data: {
             playersData: playersData,
+            teamsData: teamsData,
             gameDates: gameDates
         }
     };
 }
 
-function getPlayerRankingData(leagueId, playersData) {
-    var leaguePlayers = getLeaguePlayersByLeagueId(leagueId);
+function getEntityRankingData(leagueId, playersData, type) {
+    var leagueEntities = getLeagueEntitiesByLeagueId(leagueId, type);
 
-    //Filters players not having played in the X last games
+    //Filters entities not having played in the X last games
     var firstRankingGame = getFirstRankingGame(leagueId);
     if (firstRankingGame) {
         var timeMin = firstRankingGame.time;
-        leaguePlayers = leaguePlayers.filter(function (leaguePlayer) {
-            var lastGamePlayer = getLastGamePlayerByLeagueIdPlayerId(leagueId, leaguePlayer.playerId);
-            return lastGamePlayer && lastGamePlayer.time.localeCompare(timeMin) >= 0;
+        leagueEntities = leagueEntities.filter(function (leagueEntity) {
+            var lastGameEntity = getLastGameEntityByLeagueIdEntityId(leagueId, leagueEntity[getIdName(type)], type);
+            return lastGameEntity && lastGameEntity.time.localeCompare(timeMin) >= 0;
         });
     }
 
-    //Adapt for ramping users
-    leaguePlayers.forEach(function (leaguePlayer) {
-        var gamesCount = getGamesCountByLeagueIdPlayerId(leagueId, leaguePlayer.playerId);
-        leaguePlayer.gamesCount = gamesCount;
-        leaguePlayer.rampingCoef = getRampingCoef(gamesCount);
-        leaguePlayer.rampedRating = leaguePlayer.rating * leaguePlayer.rampingCoef;
+    //Adapt for ramping entities
+    leagueEntities.forEach(function (leagueEntity) {
+        var gamesCount = getGamesCountByLeagueIdPlayerId(leagueId, leagueEntity[getIdName(type)]);
+        leagueEntity.gamesCount = gamesCount;
+        leagueEntity.rampingCoef = getRampingCoef(gamesCount);
+        leagueEntity.rampedRating = leagueEntity.rating * leagueEntity.rampingCoef;
     });
 
     //Return only useful information
-    leaguePlayers.forEach(function (leaguePlayer) {
-        playersData[leaguePlayer.playerId] = {
-            rating: leaguePlayer.rating,
-            rampedRating: leaguePlayer.rampedRating,
-            rampingCoef: leaguePlayer.rampingCoef,
-            gamesCount: leaguePlayer.gamesCount
+    leagueEntities.forEach(function (leagueEntity) {
+        playersData[leagueEntity[getIdName(type)]] = {
+            rating: leagueEntity.rating,
+            rampedRating: leagueEntity.rampedRating,
+            rampingCoef: leagueEntity.rampingCoef,
+            gamesCount: leagueEntity.gamesCount
         };
     });
+}
+
+function getIdName(type) {
+    if (type === 'Player') {
+        return 'playerId';
+    } else {
+        return 'teamId';
+    }
 }
 
 function getRampingCoef(gamesCount) {
     return STARTING_RAMPING_COEF + (1 - STARTING_RAMPING_COEF) * Math.min(RAMPING_DURATION, gamesCount) / RAMPING_DURATION;
 }
 
-function getPlayersData(playersData) {
-    for (var playerId in playersData) {
-        var player = storeLib.getByKey(playerId);
-        playersData[playerId].name = player && player.name;
-        playersData[playerId].imageUrl = player && '/players/image/' + player._versionKey + '/' + encodeURIComponent(player.name);
+function getEntitiesData(entitiesData, type) {
+    for (var entityId in entitiesData) {
+        var entity = storeLib.getByKey(entityId);
+        entitiesData[entityId].name = entity && entity.name;
+        entitiesData[entityId].imageUrl = entity && '/' + type.toLowerCase() + 's/image/' + entity._versionKey + '/' + encodeURIComponent(entity.name);
     }
 }
 
-function getPlayerRatingData(leagueId, playersData, gameDates) {
+function getEntityRatingData(leagueId, playersData, teamsData, gameDates) {
     var currentRatings = {};
     for (var playerId in playersData) {
         playersData[playerId].ratings = [];
         playersData[playerId].ratings[GAME_RANGE] = playersData[playerId].rating;
         currentRatings[playerId] = playersData[playerId].rating;
     }
+    for (var teamId in teamsData) {
+        teamsData[teamId].ratings = [];
+        teamsData[teamId].ratings[GAME_RANGE] = teamsData[teamId].rating;
+        currentRatings[teamId] = teamsData[teamId].rating;
+    }
 
-    var games =getGamesByLeagueId(leagueId);
+    var games = getGamesByLeagueId(leagueId);
 
     if (games) {
         var gameIndex = GAME_RANGE - 1;
@@ -99,8 +116,14 @@ function getPlayerRatingData(leagueId, playersData, gameDates) {
             getGamePlayersByGameId(game._id).map(function (gamePlayer) {
                 currentRatings[gamePlayer.playerId] -= gamePlayer.ratingDelta;
             });
+            getGameTeamsByGameId(game._id).map(function (gameTeam) {
+                currentRatings[gameTeam.teamId] -= gameTeam.ratingDelta;
+            });
             for (var playerId in playersData) {
                 playersData[playerId].ratings[gameIndex] = currentRatings[playerId];
+            }
+            for (var teamId in teamsData) {
+                teamsData[teamId].ratings[gameIndex] = currentRatings[teamId];
             }
             gameDates[gameIndex] = game.time.substr(0, 10);
             gameIndex--;
@@ -108,9 +131,9 @@ function getPlayerRatingData(leagueId, playersData, gameDates) {
     }
 }
 
-function getLeaguePlayersByLeagueId(leagueId) {
+function getLeagueEntitiesByLeagueId(leagueId, type) {
     return storeLib.get({
-        query: 'type="leaguePlayer" AND leagueId="' + leagueId + '"',
+        query: 'type="league' + type + '" AND leagueId="' + leagueId + '"',
         count: 1024
     });
 }
@@ -120,6 +143,13 @@ function getGamePlayersByGameId(gameId) {
         query: 'type="gamePlayer" AND gameId="' + gameId + '"',
         count: 4
     });
+}
+
+function getGameTeamsByGameId(gameId) {
+    return storeLib.get({
+        query: 'type="gameTeam" AND gameId="' + gameId + '"',
+        count: 2
+    }) || [];
 }
 
 function getGamesByLeagueId(leagueId) {
@@ -147,9 +177,9 @@ function getFirstRankingGame(leagueId) {
     });
 }
 
-function getLastGamePlayerByLeagueIdPlayerId(leagueId, playerId) {
+function getLastGameEntityByLeagueIdEntityId(leagueId, entityId, type) {
     return storeLib.get({
-        query: 'type="gamePlayer" AND leagueId="' + leagueId + '" AND playerId="' + playerId + '"',
+        query: 'type="game' + type + '" AND leagueId="' + leagueId + '" AND ' + getIdName(type) + '="' + entityId + '"',
         count: 1,
         sort: 'time DESC'
     });
